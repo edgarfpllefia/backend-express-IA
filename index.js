6a206495e8f615b1809ee97937099acc91ccedd3
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-05-11 12:28:12 +0200

    cors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './db/connect.js';
-import bacallaRouter from './routes/bacalla.js';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/connect.js";
+import bacallaRouter from "./routes/bacalla.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/bacalla', bacallaRouter);
+app.use("/api/bacalla", bacallaRouter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
